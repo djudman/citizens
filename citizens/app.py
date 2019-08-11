b838b3ca8cd4d1f71921613b3a3d497dbb0277b7
Dmitrii Dorofeev <djudman@gmail.com>
2019-08-11 17:34:31 +0300

    Some config changes (unix sockets files moved to /tmp) Now class CitizensRestApi includes creation aiohttp app, closing unix socket at shutdown.

--- citizens/app.py
+++ citizens/app.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import logging
 import logging.config
@@ -34,47 +35,50 @@ async def shutdown(app):
     app.storage.close()
 
 
-def load_config():  # TODO: сделать лучше
-    filename = 'citizens.config.json'
-    search_path = (dirname(dirname(__file__)), '~', '~/.config')
-    for dir_name in search_path:
-        filepath = join(realpath(expanduser(dir_name)), filename)
-        if exists(filepath):
-            work_dir = os.getcwd()
-            os.chdir(realpath(dirname(filepath)))
-            # TODO: контекстный менеджер про "перейти в директорию с конфигом"
-            with open(filepath) as f:
-                config = json.load(f)
-                log_dir = realpath(dirname(config['logging']['handlers']['citizens_file']['filename']))
-                if not exists(log_dir):
-                    makedirs(log_dir, exist_ok=True)
-                logging.config.dictConfig(config['logging'])
-                os.chdir(work_dir)
-                return config
-    return {
-        'logging': {'version': 1},
-    }
-
-
-def create_app():
-    load_config()
-    app = web.Application(
-        logger=logging.getLogger('citizens'),
-        middlewares=[errors_middleware]
-    )
-    app.storage = MongoStorage({'db': 'citizens'})
-    app.add_routes([
-        web.post('/imports', new_import),
-        web.patch(r'/imports/{import_id:\d+}/citizens/{citizen_id:\d+}', update_citizen),
-        web.get(r'/imports/{import_id:\d+}/citizens', get_citizens),
-        web.get(r'/imports/{import_id:\d+}/citizens/birthdays', get_presents_by_month),
-        web.get(r'/imports/{import_id:\d+}/towns/stat/percentile/age', get_age_percentiles),
-    ])
-    app.on_cleanup.append(shutdown)
-    return app
-
-
 class CitizensRestApi:
+    def __init__(self):
+        self._unix_socket = None
+        self._logger = logging.getLogger('citizens')
+        self._config = self._load_config()
+        self._app = self._create_app()
+
+    def _load_config(self):  # TODO: сделать лучше
+        filename = 'citizens.config.json'
+        search_path = (dirname(dirname(__file__)), '~', '~/.config')
+        for dir_name in search_path:
+            filepath = join(realpath(expanduser(dir_name)), filename)
+            if exists(filepath):
+                work_dir = os.getcwd()
+                os.chdir(realpath(dirname(filepath)))
+                # TODO: контекстный менеджер про "перейти в директорию с конфигом"
+                with open(filepath) as f:
+                    config = json.load(f)
+                    log_dir = realpath(dirname(config['logging']['handlers']['citizens_file']['filename']))
+                    if not exists(log_dir):
+                        makedirs(log_dir, exist_ok=True)
+                    logging.config.dictConfig(config['logging'])
+                    os.chdir(work_dir)
+                    return config
+        return {
+            'logging': {'version': 1},
+        }
+
+    def _create_app(self):
+        app = web.Application(
+            logger=logging.getLogger('citizens'),
+            middlewares=[errors_middleware]
+        )
+        app.storage = MongoStorage({'db': 'citizens'})
+        app.add_routes([
+            web.post('/imports', new_import),
+            web.patch(r'/imports/{import_id:\d+}/citizens/{citizen_id:\d+}', update_citizen),
+            web.get(r'/imports/{import_id:\d+}/citizens', get_citizens),
+            web.get(r'/imports/{import_id:\d+}/citizens/birthdays', get_presents_by_month),
+            web.get(r'/imports/{import_id:\d+}/towns/stat/percentile/age', get_age_percentiles),
+        ])
+        app.on_cleanup.append(self._shutdown)
+        return app
+
     def run(self, port=8080, unix_socket_path=None):
         sock = None
         if unix_socket_path is not None:
@@ -83,6 +87,20 @@ class CitizensRestApi:
                 os.makedirs(socket_dirpath, exist_ok=True)
             sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
             sock.bind(unix_socket_path)
-        app = create_app()
-        web.run_app(app, print=None, port=port, sock=sock)
+            self._unix_socket = (unix_socket_path, sock)
+            self._logger.info(f'Unix socket `{unix_socket_path}` is ready.')
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        web.run_app(self._app, print=None, port=port, sock=sock)
 
+    async def _shutdown(self, app):
+        app.storage.close()
+        if self._unix_socket is not None:
+            filepath, sock = self._unix_socket
+            if sock.fileno() != -1:
+                sock.shutdown(socket.SHUT_RDWR)
+                sock.close()
+                self._logger.info(f'Unix socket `{filepath}` closed.')
+            if exists(filepath):
+                os.unlink(filepath)
+                self._logger.info(f'File `{filepath}` deleted.')
+        self._logger.info('Application is shutdown.')
