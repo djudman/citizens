caf0b19d21f904ba94eeaf02d2e96b6575459473
Dmitrii Dorofeev <dorofeev@game-insight.com>
2019-08-15 15:10:54 +0300

    Server address in Makefile updated. Some fixes in debug script tests/scripts/client.py.

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-host:=84.201.138.48
+host:=84.201.176.147  # тестовый стенд
+# host:=127.0.0.1 # пригождается для отладки
 
 all:
 	@./venv/citizens/bin/python setup.py sdist
--- tests/scripts/client.py
+++ tests/scripts/client.py
@@ -11,16 +11,13 @@ from data import ImportDataGenerator
 
 
 class CitizensRestApiClient:
-    def __init__(self, host='127.0.0.1', port=8080, timeout=10):
-        self._host = host
-        self._port = port
-        self._timeout = timeout
+    def __init__(self, host='127.0.0.1', port=8080):
+        self._target_host = f'{host}:{port}'
 
     async def async_http_request(self, http_method='GET', uri='/', body=None):
         t1 = time.time()
-        target_host = '{0}:{1}'.format(self._host, self._port)
         async with aiohttp.ClientSession() as session:
-            url = f'http://{target_host}{uri}'
+            url = 'http://{target_host}{uri}'.format(target_host=self._target_host, uri=uri)
             do_request = getattr(session, http_method.lower())
             if body is None:
                 do_request = functools.partial(do_request, url)
@@ -33,39 +30,45 @@ class CitizensRestApiClient:
                 print('{0} took {1}'.format(response_data, time.time() - t1))
                 return resp.status, response_data
 
-    async def import_data(self, num_citizens=10000):
+    async def import_data(self, num_imports=1, num_citizens=10000):
+        t1 = time.time()
         generator = ImportDataGenerator()
         data = json.dumps(list(generator.generate_import_data(num_citizens)))
         requests = []
-        for _ in range(10):
+        for _ in range(num_imports):
             requests.append(
-                asyncio.create_task(self.async_http_request('POST', '/imports', body=data))
+                asyncio.create_task(
+                    self.async_http_request('POST', '/imports', body=data)
+                )
             )
         responses = await asyncio.gather(*requests)
+        duration = time.time() - t1
+        print(f'Duration of {num_imports} imports: {duration} sec')
         return responses
 
-    def import_and_break_connection(self):
+    def import_and_break_connection(self, num_citizens=1000):
+        print(self._target_host)
         generator = ImportDataGenerator()
-        data = json.dumps(list(generator.generate_import_data(num_citizens=30000)))
-        connection = HTTPConnection(host, port)
-        connection.request('POST', '/imports', json.dumps(data))
+        data = json.dumps(list(generator.generate_import_data(num_citizens)))
+        connection = HTTPConnection(self._target_host)
+        connection.request('POST', '/imports', data)
         connection.close()
         # TODO: пойти на сервер и убедиться, что все записи вставились
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Citizens REST API Test Client")
-    parser.add_argument('--host')
-    parser.add_argument('--port')
+    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('--port', default=8080)
     args = parser.parse_args()
+
     host = args.host
-    port = args.port or (host and 80) or None
+    port = args.port
+    if host != '127.0.0.1':
+        port = 80
 
     client = CitizensRestApiClient(host=host, port=port)
-    t1 = time.time()
+    ############################################################################
 
-    responses = asyncio.run(client.import_data())
-    print('Requests duration: {0}'.format(time.time() - t1))
-    print(responses)
-
-    # client.import_and_break_connection()
+    print(asyncio.run(client.import_data(num_imports=2)))
+    # client.import_and_break_connection(num_citizens=30000)
