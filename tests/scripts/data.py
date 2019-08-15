1bcf6f299f8a7e9fb9bee37d654f84dd868bacac
Dmitrii Dorofeev <dorofeev@game-insight.com>
2019-08-15 20:08:57 +0300

    README updated. Comment added to nginx.conf. Scripts for generating test data updated (ammo.py, data.py). Yandex tank config template updated.

--- tests/scripts/data.py
+++ tests/scripts/data.py
@@ -6,7 +6,8 @@ import string
 
 class ImportDataGenerator:
     def __init__(self):
-        self._counter = 1
+        self._next_citizen_id = 1
+        self._relatives = {}
 
     def _generate_string(self, min_length=1, max_length=50):
         return ''.join(
@@ -20,15 +21,26 @@ class ImportDataGenerator:
         day = random.randint(1, 28)
         return datetime.datetime(year=year, month=month, day=day).strftime('%d.%m.%Y')
 
-    def _generate_relatives(self, is_orphan=False):
-        if is_orphan:
-            return []
-        return []
+    def _generate_relatives(self, citizen_id, max_relative_id):
+        num_relatives = random.randint(1, 4)
+        relative_id = None
+        if citizen_id not in self._relatives:
+            self._relatives[citizen_id] = set()
+        for _ in range(num_relatives):
+            while not relative_id or (relative_id in self._relatives[citizen_id]):
+                relative_id = random.randint(1, max_relative_id)
+            self._relatives[citizen_id].add(relative_id)
+            if relative_id not in self._relatives:
+                self._relatives[relative_id] = set()
+            self._relatives[relative_id].add(citizen_id)
 
-    def generate_citizen_data(self):
-        self._counter += 1
+    def generate_citizen_data(self, max_citizen_id):
+        citizen_id = self._next_citizen_id
+        self._next_citizen_id += 1
+        if citizen_id not in self._relatives and random.randint(1, 100) > 50:
+            self._generate_relatives(citizen_id, max_citizen_id)
         return {
-            'citizen_id': self._counter,
+            'citizen_id': citizen_id,
             'town': self._generate_string(5, 15),
             'street': self._generate_string(10, 20),
             'building': self._generate_string(2, 6),
@@ -36,8 +48,15 @@ class ImportDataGenerator:
             'name': self._generate_string(4, 10),
             'birth_date': self._generate_date(),
             'gender': random.choice(['male', 'female']),
-            'relatives': self._generate_relatives(random.randint(1, 100) > 50),
+            'relatives': [],
         }
 
     def generate_import_data(self, num_citizens=100):
-        return (self.generate_citizen_data() for _ in range(num_citizens))
+        self._next_citizen_id = 1
+        self._relatives = {}
+        citizens = [self.generate_citizen_data(num_citizens) for _ in range(num_citizens)]
+        for data in citizens:
+            citizen_id = data['citizen_id']
+            if citizen_id in self._relatives:
+                data['relatives'] = list(self._relatives[citizen_id])
+            yield data
--- tests/scripts/yandex-tank/ammo.py
+++ tests/scripts/yandex-tank/ammo.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import random
 import sys
 from os.path import dirname, realpath, join
 
@@ -14,30 +15,105 @@ def create_ammo(target_host='localhost', num_imports=3, num_citizens_per_import=
     generator = ImportDataGenerator()
     imports = (generator.generate_import_data(num_citizens_per_import)
                for _ in range(num_imports))
+
+    import_requests = []
     requests = []
-    for import_data in imports:
-        str_import_data = json.dumps(list(import_data))
-        content_length = len(str_import_data)
-        headers = '\r\n'.join([
-            'POST /imports HTTP/1.1',
-            'Content-Length: {0}'.format(content_length),
-            'Host: {0}'.format(target_host),
-            'User-Agent: yandex-tank'
-        ])
-        request = '{request_headers}\r\n\r\n{body}'.format(
-            request_headers=headers,
-            body=str_import_data
-        )
-        requests.append(
-            '{request_size} {tag}\n{request}'.format(
-                request_size=len(request),
-                tag='import',
-                request=request
-            )
-        )
+    for index, import_data in enumerate(imports):
+        import_id = index + 1
+        import_request = create_import_request(target_host, import_data)
+        import_requests.append(import_request)
+        for _ in range(num_citizens_per_import // 2):
+            r = create_patch_citizen_request(target_host, import_id, num_citizens_per_import)
+            requests.append(r)
+        for _ in range(num_citizens_per_import // 4):
+            r = create_get_all_citizens_request(target_host, import_id)
+            requests.append(r)
+        for _ in range(num_citizens_per_import // 4):
+            r = create_get_birthdays_request(target_host, import_id)
+            requests.append(r)
+        for _ in range(num_citizens_per_import // 3):
+            r = create_get_percentiles_request(target_host, import_id)
+            requests.append(r)
+        random.shuffle(requests)
+    for r in import_requests:
+        requests.insert(0, r)
+
     with open(filename, 'w') as f:
         f.write('\n'.join(requests))
     return filename
+
+
+def create_http_request(target_host, method, uri, data=None, tag='request'):
+    str_data = json.dumps(data) if data is not None else ''
+    content_length = len(str_data)
+    headers = '\r\n'.join([
+        '{0} {1} HTTP/1.1'.format(method, uri),
+        'Content-Length: {0}'.format(content_length),
+        'Host: {0}'.format(target_host),
+        'User-Agent: yandex-tank'
+    ])
+    request = '{request_headers}\r\n\r\n{body}'.format(
+        request_headers=headers,
+        body=str_data
+    )
+    return '{request_size} {tag}\n{request}'.format(
+        request_size=len(request),
+        tag=tag,
+        request=request
+    )
+
+
+def create_import_request(target_host, import_data):
+    return create_http_request(
+        target_host,
+        method='POST',
+        uri='/imports',
+        data=list(import_data),
+        tag='import'
+    )
+
+
+def create_patch_citizen_request(target_host, import_id, num_citizens):
+    citizen_id = random.randint(2, num_citizens)
+    new_values = {
+        'name': 'Updated',
+        'relatives': [random.randint(1, num_citizens)]
+    }
+    patch_citizen_request = create_http_request(
+        target_host,
+        method='PATCH',
+        uri=f'/imports/{import_id}/citizens/{citizen_id}',
+        data=new_values,
+        tag='patch_citizen'
+    )
+    return patch_citizen_request
+
+
+def create_get_all_citizens_request(target_host, import_id):
+    return create_http_request(
+        target_host,
+        method='GET',
+        uri=f'/imports/{import_id}/citizens',
+        tag='get_all_citizens'
+    )
+
+
+def create_get_birthdays_request(target_host, import_id):
+    return create_http_request(
+        target_host,
+        method='GET',
+        uri=f'/imports/{import_id}/citizens/birthdays',
+        tag='get_birthdays'
+    )
+
+
+def create_get_percentiles_request(target_host, import_id):
+    return create_http_request(
+        target_host,
+        method='GET',
+        uri=f'/imports/{import_id}/towns/stat/percentile/age',
+        tag='get_percentiles'
+    )
 
 
 if __name__ == '__main__':
@@ -49,7 +125,7 @@ if __name__ == '__main__':
         sys.exit(1)
     create_ammo(
         target_host=args.host,
-        num_imports=10,
+        num_imports=2,
         num_citizens_per_import=10000,
         filename=join(realpath(dirname(__file__)), 'ammo.txt')
     )
