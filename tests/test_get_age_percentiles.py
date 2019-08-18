140705d2d4cddcdcef41f5f9e119698a2ca05ab5
Dmitrii Dorofeev <djudman@gmail.com>
2019-08-18 18:05:52 +0300

    Calculated percentiles by hand (for checking) and rewrote the test.

--- tests/test_get_age_percentiles.py
+++ tests/test_get_age_percentiles.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 from aiohttp.test_utils import unittest_run_loop
@@ -6,66 +7,42 @@ from tests.utils import CitizensApiTestCase
 
 
 class TestGetAgePercentiles(CitizensApiTestCase):
+    def setUp(self):
+        super().setUp()
+        self._counter = 1
+
+    def _make_citizen_data(self, town, age):
+        now = datetime.datetime.now()
+        birth_date = datetime.date(year=(now.year - age), month=now.month, day=now.day)
+        data = {
+            'citizen_id': self._counter,
+            'town': town,
+            'street': 'Ленина',
+            'building': '1',
+            'apartment': 1,
+            'name': 'Иванов',
+            'birth_date': birth_date.strftime('%d.%m.%Y'),
+            'gender': 'male',
+            'relatives': [],
+        }
+        self._counter += 1
+        return data
+
     @unittest_run_loop
     async def test_get_percentiles(self):
-        import_id = await self.import_data([
-            {
-                "citizen_id": 1,
-                "town": "Москва",
-                "street": "Льва Толстого",
-                "building": "16к7стр5",
-                "apartment": 7,
-                "name": "Иванов Сергей Иванович",
-                "birth_date": "17.04.1999",
-                "gender": "male",
-                "relatives": [2]
-            },
-            {
-                "citizen_id": 2,
-                "town": "Москва",
-                "street": "Льва Толстого",
-                "building": "16к7стр5",
-                "apartment": 8,
-                "name": "Иванов Иван Иванович",
-                "birth_date": "07.05.1989",
-                "gender": "male",
-                "relatives": [1]
-            },
-            {
-                "citizen_id": 3,
-                "town": "Москва",
-                "street": "Льва Толстого",
-                "building": "16к7стр5",
-                "apartment": 8,
-                "name": "Иванов Иван Иванович",
-                "birth_date": "07.05.1979",
-                "gender": "male",
-                "relatives": []
-            },
-            {
-                "citizen_id": 4,
-                "town": "Санкт-Петербург",
-                "street": "Льва Толстого",
-                "building": "16к7стр5",
-                "apartment": 8,
-                "name": "Иванов Иван Иванович",
-                "birth_date": "07.05.1949",
-                "gender": "male",
-                "relatives": []
-            },
-            {
-                "citizen_id": 5,
-                "town": "Санкт-Петербург",
-                "street": "Льва Толстого",
-                "building": "16к7стр5",
-                "apartment": 8,
-                "name": "Иванов Иван Иванович",
-                "birth_date": "07.05.1959",
-                "gender": "male",
-                "relatives": []
-            }
-        ])
-        status, data = await self.api_request('GET', f'/imports/{import_id}/towns/stat/percentile/age')
+        citizens = [
+            self._make_citizen_data('Москва', 15),
+            self._make_citizen_data('Москва', 20),
+            self._make_citizen_data('Москва', 62),
+            self._make_citizen_data('Санкт-Петербург', 5),
+            self._make_citizen_data('Санкт-Петербург', 32),
+            self._make_citizen_data('Санкт-Петербург', 32),
+            self._make_citizen_data('Санкт-Петербург', 59),
+            self._make_citizen_data('Санкт-Петербург', 65),
+        ]
+        import_id = await self.import_data(citizens)
+        status, data = await self.api_request(
+            'GET', f'/imports/{import_id}/towns/stat/percentile/age')
         self.assertEquals(status, 200)
         self.assertIsNotNone(data)
         self.assertIn('data', data)
@@ -75,17 +52,18 @@ class TestGetAgePercentiles(CitizensApiTestCase):
         self.assertEquals(len(stat), 2)
 
         self.assertEquals(stat[0]['town'], 'Москва')
-        self.assertEquals(stat[0]['p50'], 30)
-        self.assertEquals(stat[0]['p75'], 35)
-        self.assertEquals(stat[0]['p99'], 39.8)
+        self.assertEquals(stat[0]['p50'], 20)
+        self.assertEquals(stat[0]['p75'], 41)
+        self.assertEquals(stat[0]['p99'], 61.16)
 
         self.assertEquals(stat[1]['town'], 'Санкт-Петербург')
-        self.assertEquals(stat[1]['p50'], 65)
-        self.assertEquals(stat[1]['p75'], 67.5)
-        self.assertEquals(stat[1]['p99'], 69.9)
+        self.assertEquals(stat[1]['p50'], 32)
+        self.assertEquals(stat[1]['p75'], 59)
+        self.assertEquals(stat[1]['p99'], 64.76)
 
     @unittest_run_loop
     async def test_import_does_not_exists(self):
         import_id = 100
-        status, _ = await self.api_request('GET', f'/imports/{import_id}/towns/stat/percentile/age')
+        status, _ = await self.api_request(
+            'GET', f'/imports/{import_id}/towns/stat/percentile/age')
         self.assertEquals(status, 400)
