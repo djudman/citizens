15a64984891395f15c81da5a85c16f9c84e68ea7
Dmitrii Dorofeev <djudman@gmail.com>
2019-08-18 19:01:42 +0300

    A couple of tests for `get_presents_by_month()`

--- tests/test_get_presents_by_month.py
+++ tests/test_get_presents_by_month.py
@@ -7,7 +7,7 @@ from tests.utils import CitizensApiTestCase
 
 class TestGetPresentsByMonth(CitizensApiTestCase):
     @unittest_run_loop
-    async def test_citizen_birthdays(self):
+    async def test_self_present(self):
         import_id = await self.import_data([
             {
                 "citizen_id": 1,
@@ -15,8 +15,39 @@ class TestGetPresentsByMonth(CitizensApiTestCase):
                 "street": "Льва Толстого",
                 "building": "16к7стр5",
                 "apartment": 7,
-                "name": "Иванов Сергей Иванович",
-                "birth_date": "17.04.1997",
+                "name": "Иван",
+                "birth_date": "17.06.1997",
+                "gender": "male",
+                "relatives": [1]
+            }
+        ])
+        status, data = await self.api_request(
+            'GET', f'/imports/{import_id}/citizens/birthdays')
+        self.assertEquals(status, 200)
+
+        presents_by_month = data['data']
+        stat = presents_by_month['6']
+        self.assertEquals(len(stat), 1)
+        self.assertEquals(stat[0]['citizen_id'], 1)
+        self.assertEquals(stat[0]['presents'], 1)
+
+        empty_months = [str(i + 1) for i in range(12) if (i + 1) != 6]
+        for month in empty_months:
+            self.assertIn(month, presents_by_month)
+            self.assertIsInstance(presents_by_month[month], list)
+            self.assertEqual(len(presents_by_month[month]), 0)
+
+    @unittest_run_loop
+    async def test_multiple_presents_same_month(self):
+        import_id = await self.import_data([
+            {
+                "citizen_id": 1,
+                "town": "Москва",
+                "street": "Льва Толстого",
+                "building": "16к7стр5",
+                "apartment": 7,
+                "name": "Иван",
+                "birth_date": "17.06.1997",
                 "gender": "male",
                 "relatives": [2, 3]
             },
@@ -26,10 +57,10 @@ class TestGetPresentsByMonth(CitizensApiTestCase):
                 "street": "Льва Толстого",
                 "building": "16к7стр5",
                 "apartment": 8,
-                "name": "Иванов Иван Иванович",
-                "birth_date": "07.05.1987",
+                "name": "Василий",
+                "birth_date": "07.03.1987",
                 "gender": "male",
-                "relatives": [1, 2]
+                "relatives": [1]
             },
             {
                 "citizen_id": 3,
@@ -37,13 +68,72 @@ class TestGetPresentsByMonth(CitizensApiTestCase):
                 "street": "Льва Толстого",
                 "building": "16к7стр5",
                 "apartment": 8,
-                "name": "Иванов Иван Иванович",
-                "birth_date": "17.01.1987",
+                "name": "Петр",
+                "birth_date": "17.03.1987",
                 "gender": "male",
                 "relatives": [1]
             }
         ])
-        status, data = await self.api_request('GET', f'/imports/{import_id}/citizens/birthdays')
+        status, data = await self.api_request(
+            'GET', f'/imports/{import_id}/citizens/birthdays')
+        self.assertEquals(status, 200)
+
+        presents_by_month = data['data']
+        stat = presents_by_month['3']
+        self.assertEquals(len(stat), 1)
+        self.assertEquals(stat[0]['citizen_id'], 1)
+        self.assertEquals(stat[0]['presents'], 2)
+
+    @unittest_run_loop
+    async def test_citizen_birthdays(self):
+        import_id = await self.import_data([
+            {
+                "citizen_id": 1,
+                "town": "Москва",
+                "street": "Льва Толстого",
+                "building": "16к7стр5",
+                "apartment": 7,
+                "name": "Иван",
+                "birth_date": "17.04.1997",
+                "gender": "male",
+                "relatives": [3, 4]
+            },
+            {
+                "citizen_id": 2,
+                "town": "Москва",
+                "street": "Льва Толстого",
+                "building": "16к7стр5",
+                "apartment": 8,
+                "name": "Василий",
+                "birth_date": "07.05.1987",
+                "gender": "male",
+                "relatives": [3]
+            },
+            {
+                "citizen_id": 3,
+                "town": "Москва",
+                "street": "Льва Толстого",
+                "building": "16к7стр5",
+                "apartment": 8,
+                "name": "Петр",
+                "birth_date": "17.01.1987",
+                "gender": "male",
+                "relatives": [1, 2]
+            },
+            {
+                "citizen_id": 4,
+                "town": "Москва",
+                "street": "Льва Толстого",
+                "building": "16к7стр5",
+                "apartment": 8,
+                "name": "Дмитрий",
+                "birth_date": "01.01.1987",
+                "gender": "male",
+                "relatives": [1, 4]
+            }
+        ])
+        status, data = await self.api_request(
+            'GET', f'/imports/{import_id}/citizens/birthdays')
         self.assertEquals(status, 200)
         self.assertIsNotNone(data)
         self.assertIn('data', data)
@@ -51,29 +141,33 @@ class TestGetPresentsByMonth(CitizensApiTestCase):
         presents_by_month = data['data']
         self.assertIsInstance(presents_by_month, dict)
 
-        self.assertEquals(len(presents_by_month['1']), 1)
-        citizen = presents_by_month['1'][0]
-        self.assertEquals(citizen['citizen_id'], 1)
-        self.assertEquals(citizen['presents'], 1)
+        for i in range(12):
+            month = str(i + 1)
+            self.assertIn(month, presents_by_month)
+
+        stat = sorted(presents_by_month['1'], key=lambda data: data['citizen_id'])
+        self.assertEquals(len(stat), 3)
+        self.assertEquals(stat[0]['citizen_id'], 1)
+        self.assertEquals(stat[0]['presents'], 2)
+        self.assertEquals(stat[1]['citizen_id'], 2)
+        self.assertEquals(stat[1]['presents'], 1)
+        self.assertEquals(stat[2]['citizen_id'], 4)
+        self.assertEquals(stat[2]['presents'], 1)
 
         self.assertEquals(len(presents_by_month['2']), 0)
         self.assertEquals(len(presents_by_month['3']), 0)
 
-        self.assertEquals(len(presents_by_month['4']), 2)
-        citizen = presents_by_month['4'][0]
-        self.assertEquals(citizen['citizen_id'], 2)
-        self.assertEquals(citizen['presents'], 1)
-        citizen = presents_by_month['4'][1]
-        self.assertEquals(citizen['citizen_id'], 3)
-        self.assertEquals(citizen['presents'], 1)
+        stat = sorted(presents_by_month['4'], key=lambda data: data['citizen_id'])
+        self.assertEquals(len(stat), 2)
+        self.assertEquals(stat[0]['citizen_id'], 3)
+        self.assertEquals(stat[0]['presents'], 1)
+        self.assertEquals(stat[1]['citizen_id'], 4)
+        self.assertEquals(stat[1]['presents'], 1)
 
-        self.assertEquals(len(presents_by_month['5']), 2)
-        citizen = presents_by_month['5'][0]
-        self.assertEquals(citizen['citizen_id'], 1)
-        self.assertEquals(citizen['presents'], 1)
-        citizen = presents_by_month['5'][1]
-        self.assertEquals(citizen['citizen_id'], 2)
-        self.assertEquals(citizen['presents'], 1)
+        stat = sorted(presents_by_month['5'], key=lambda data: data['citizen_id'])
+        self.assertEquals(len(stat), 1)
+        self.assertEquals(stat[0]['citizen_id'], 3)
+        self.assertEquals(stat[0]['presents'], 1)
 
         for month in range(6, 13):
             self.assertEquals(len(presents_by_month[str(month)]), 0)
