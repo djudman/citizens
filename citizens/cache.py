94f216cc407a8f9e1ae19f765e2027f4c301a597
Dmitrii Dorofeev <djudman@gmail.com>
2019-08-25 15:35:49 +0300

    Some fixes in cache (for concurrency). README updated.

--- citizens/cache.py
+++ citizens/cache.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import shutil
 from os.path import exists, join, dirname
@@ -46,6 +47,7 @@ class CitizensFileCache:
     def __init__(self, max_size=10):
         self._max_size = max_size
         self._citizens_by_import = {}
+        self._logger = logging.getLogger('citizens')
 
     def _get_cache_path(self, import_id=None, key=None):
         cache_path = '/tmp/citizens.cache'
@@ -57,26 +59,39 @@ class CitizensFileCache:
 
     def put(self, import_id, key, data):
         filepath = self._get_cache_path(import_id, key)
-        if exists(filepath):
-            os.unlink(filepath)
-        else:
-            cache_dir = dirname(filepath)
-            os.makedirs(cache_dir, exist_ok=True)
-        with open(filepath, 'wb') as f:
-            f.write(data)
+        try:
+            if exists(filepath):
+                os.unlink(filepath)
+            else:
+                cache_dir = dirname(filepath)
+                os.makedirs(cache_dir, exist_ok=True)
+            with open(filepath, 'xb') as f:
+                f.write(data)
+        except Exception:
+            self._logger.error(f'Put to cache failed. `{filepath}`', exc_info=True)
 
     def get(self, import_id, key):
         filepath = self._get_cache_path(import_id, key)
         if exists(filepath):
-            with open(filepath, 'rb') as f:
-                return f.read()
+            try:
+                # тут внезапно файла может не оказаться, потому что его удалил другой процесс
+                with open(filepath, 'rb') as f:
+                    return f.read()
+            except Exception:
+                self._logger.error(f'Cannot read `{filepath}`', exc_info=True)
     
     def clear(self, import_id):
         import_cache_dir = self._get_cache_path(import_id)
         if exists(import_cache_dir):
-            shutil.rmtree(import_cache_dir)
+            try:
+                shutil.rmtree(import_cache_dir)
+            except OSError:
+                self._logger.error(f'Failed delete directory `{import_cache_dir}`', exc_info=True)
 
     def close(self):
         cache_dir = self._get_cache_path()
         if exists(cache_dir):
-            shutil.rmtree(cache_dir)
+            try:
+                shutil.rmtree(cache_dir)
+            except OSError:
+                self._logger.error(f'Failed delete directory `{cache_dir}`', exc_info=True)
